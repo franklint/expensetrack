358beff7c17d2d898721dd1ca07d541a31d29b4a
franklinT <franklintsung3@gmail.com>
2022-06-12 12:24:38 -0400

    sunday afternoon push

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,43 +1,46 @@
 const newFormHandler = async (event) => {
-    event.preventDefault(); 
+  event.preventDefault();
 
-    const name = document. querySelector('#name').value.trim(); 
-    const amount = document.querySelector('#email-signup').value.trim(); 
-    const date = document.querySelector('#pwd').value.trim();
-
-    if(name && amount && date) { 
-      const response = await fetch(`/api/expenses/newexpense`, {
-        method: 'POST', 
-        body: JSON.stringify({ name, amount, date}), 
-        headers: { 
-          'Content-Type': 'application/json', 
-        },
-      });
-
-      if(response.ok) { 
-        document.location.replace('/profile'); 
-      } else { 
-        alert('Failed to create project'); 
-      }
-    }
-};
-
-const delButtonHandler = async(event) => { 
-  if(event.target.hasAttribute('data-id')) { 
-    const id = event.target.getAttribute('data-id'); 
-
-    const response = await fetch(`/api/expenses/${id}`, {
-      method: 'DELETE', 
+  const name = document.querySelector('#item-name').value.trim();
+  const amount = document.querySelector('#item-amount').value.trim();
+  const date = document.querySelector('#item-date').value.trim();
+  if (name && amount && date) {
+    const response = await fetch(`/api/expenses/newexpense`, {
+      method: 'POST',
+      body: JSON.stringify({ name, amount, date }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
-    if(response.ok) {
-      document.locationh.replace('/profile');
-    } else { 
-      alert('Failed to delete'); 
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create project');
     }
   }
-}; 
+};
 
-document.querySelector('#submitBtn1').addEventListener('click', newFormHandler);
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
 
-document.querySelector('.deleteBtn').addEventListener('click', delButtonHandler); 
+    const response = await fetch(`/api/expenses/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete');
+    }
+  }
+};
+
+document
+  .querySelector('#submitBtn1')
+  .addEventListener('click', newFormHandler);
+
+document
+  .querySelector('.deleteBtn')
+  .addEventListener('click', delButtonHandler);
